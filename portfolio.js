7e1c580ecd28c4e764f0efe4bd3881d45f5279ed
vladapysh <vlada.pysh1@gmail.com>
2021-08-23 15:10:41 -0500

    created first carousel

--- portfolio.js
+++ portfolio.js
@@ -20,3 +20,19 @@ function clickHandler(e) {
     behavior: "smooth"
   });
 }
+
+document.body.onclick = function() {
+  document.querySelector("#carouselPortfolio").style.display = "none";
+};
+
+document.querySelector("#portfolio_item_1").addEventListener("click", function(){
+  document.querySelector("#carouselPortfolio").style.display = "block";
+});
+
+var els = document.querySelectorAll("#carouselPortfolio, #portfolio_item_1");
+// When clicking on these elements don't hide the popup
+for (var i = 0; i < els.length; i++) {
+  els[i].addEventListener("click", function(e) {
+    e.stopPropagation();
+  });
+}
